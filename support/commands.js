dc8a91acd649a4e16b613b89fcb6038f683c2c40
Thaynara Dutra <thaynara.dutra@i.systems.com.br>
2022-10-16 19:05:34 -0300

    treino API finalizado

--- support/commands.js
+++ support/commands.js
@@ -1,5 +1,5 @@
-function getEndpoint(resource) {
-    let baseEndpoint = Cypress.config().baseUrl + "/v1/";
+function getEndpoint(resource, urlParam, id) {
+    let baseEndpoint = Cypress.config().baseUrl + "/v1/public/";
   
     const resourceDict = {
       comics: "comics",
@@ -7,31 +7,34 @@ function getEndpoint(resource) {
     };
 
     if (resource === "characters") {
-        baseEndpoint = Cypress.config().baseUrl + "/v1/";
+        baseEndpoint = Cypress.config().baseUrl + "/v1/public/";
         if (id) {
-          return baseEndpoint + resourceDict[resource] + "/" + '?ts=1&apikey=70897916748e20adf1dca23fea881758&hash=1e06e99be42fbab3962c1f3de890a4d0';
+          return baseEndpoint + resourceDict[resource] + "/" + id + '?ts=1&apikey=70897916748e20adf1dca23fea881758&hash=1e06e99be42fbab3962c1f3de890a4d0';
         }
       }
       
       if (id) {
-        return baseEndpoint + resourceDict[resource] + "/" ;
+        return baseEndpoint + resourceDict[resource] + "/" + id;
       }
       return baseEndpoint + resourceDict[resource];
     }
 
-    Cypress.Commands.add(
+      Cypress.Commands.add(
         "getResource",
-        () => {
+        (
+          id,
+          resource, {
+            shouldFail = true,
+            urlParam
+          } = {}
+        ) => {
           const endpoint =
-            getEndpoint(resource) + returnOptions(options);
-                  
+            getEndpoint(resource, urlParam, id)
+      
           let requestInfo = {
             method: "GET",
             url: endpoint,
             failOnStatusCode: shouldFail,
-            headers: {
-
-            },
           };
           cy.request(requestInfo);
         }
